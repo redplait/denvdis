f3f8db0a2db93b02b970cfdbd5f5ada7b826cd01
redp <redplait@gmail.com>
2025-10-06 14:46:00 +0300

    ifelse

--- ctf/tla/plot.R
+++ ctf/tla/plot.R
@@ -8,7 +8,6 @@ if (length(args)==0) {
   # try to plot
   # p <- ggplot(data = data, aes(x = scale, y = ms)) + scale_x_continuous(trans = "log2") + geom_point()
   p <- ggplot(data = data, aes(x = scale, y = ms)) + geom_bar(stat = "identity") + scale_x_continuous(trans = "log2")
-  if (length(args)>1) { png_fname = args[2] }
-  else { png_fname = sub("csv", "png", args[1]) }
+  png_fname <- ifelse(length(args)>1, args[2], sub("csv", "png", args[1]) )
   ggsave(png_fname, plot = p)
 }
