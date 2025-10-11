0c142048f0025fac4cc335b9d711b2f993c69f6c
redp <redplait@gmail.com>
2025-10-11 14:06:48 +0300

    axes descriptions

--- ctf/tla/batch_plot.R
+++ ctf/tla/batch_plot.R
@@ -6,8 +6,10 @@ for(aidx in 1:length(args) )
   fname <- args[aidx]
   data <- read.csv(file = fname)
   # try to plot - uncomment below line to produce points plot
-  # p <- ggplot(data = data, aes(x = scale, y = ms)) + scale_x_continuous(trans = "log2") + geom_point()
+  # p <- ggplot(data = data, aes(x = scale, y = ms)) + scale_x_continuous(trans = "log2") + geom_point() +
   # to produce bar plot
-  p <- ggplot(data = data, aes(x = scale, y = ms)) + geom_bar(stat = "identity") + scale_x_continuous(trans = "log2")
+  # always good idea to add axes descriptions
+  p <- ggplot(data = data, aes(x = scale, y = ms)) + geom_bar(stat = "identity") + scale_x_continuous(trans = "log2") +
+   xlab("scale log") + ylab("time, ms")
   ggsave(sub("csv", "png", fname), plot = p)
 }
--- ctf/tla/plot.R
+++ ctf/tla/plot.R
@@ -6,8 +6,10 @@ if (length(args)==0) {
   # R has start array index 1
   data <- read.csv(file = args[1])
   # try to plot
-  # p <- ggplot(data = data, aes(x = scale, y = ms)) + scale_x_continuous(trans = "log2") + geom_point()
-  p <- ggplot(data = data, aes(x = scale, y = ms)) + geom_bar(stat = "identity") + scale_x_continuous(trans = "log2")
+  # always good idea to add axes descriptions
+  # p <- ggplot(data = data, aes(x = scale, y = ms)) + scale_x_continuous(trans = "log2") + geom_point() +
+  p <- ggplot(data = data, aes(x = scale, y = ms)) + geom_bar(stat = "identity") + scale_x_continuous(trans = "log2") +
+    xlab("scale log") + ylab("time, ms")
   png_fname <- ifelse(length(args)>1, args[2], sub("csv", "png", args[1]) )
   ggsave(png_fname, plot = p)
 }
