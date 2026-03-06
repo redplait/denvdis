f3f86fc997ee782b276ae1468c6cf8e0b37094a6
redp <redplait@gmail.com>
2026-03-06 14:07:04 +0300

    some arragment

--- scripts/pal.pl
+++ scripts/pal.pl
@@ -7,7 +7,49 @@ use warnings;
 my %g_ops;
 my $g_merc = 0;
 my $g_total = 0;
+# key is full op name
 my %g_bad;
+
+sub place
+{
+  my($name, $v) = @_;
+  if ( !exists $g_ops{$name} ) {
+    $g_ops{$name} = [ 1, $v ];
+  } else {
+    my $ar = $g_ops{$name};
+    $ar->[0]++;
+    push @$ar, ( $name, $v );
+  }
+}
+
+sub dump_good
+{
+  printf("--- good\n");
+  foreach my $n ( sort keys %g_ops ) {
+    my $v = $g_ops{$n};
+    next if ( $v->[0] != 1 );
+    printf("%s: %d\n", $n, $v->[1]);
+  }
+}
+
+sub dump_bad
+{
+  printf("--- bad %d\n", scalar keys %g_bad );
+  foreach my $n ( sort keys %g_bad ) {
+    printf("%s\n", $n);
+  }
+}
+
+sub arrange
+{
+  my ($name, $v) = @_;
+  if ( $name =~ /^\w+$/ ) { # just instruction name without '.' and spaces
+    place($name, $v);
+    return;
+  }
+  $g_bad{$name}++;
+  place($1, $v) if ( $name =~ /^(\w+)\b/ )  # extract first word
+}
 
 sub process
 {
@@ -31,6 +73,7 @@ sub process
       next;
     }
     $g_total++;
+    arrange($name, $val);
   }
   close $fh;
 }
@@ -41,3 +84,5 @@ $flat = $ARGV[0] if ( @ARGV );
 process($flat);
 printf("total %d\n", $g_total);
 printf("skipped %d mercury\n", $g_merc) if ( $g_merc );
+dump_good();
+dump_bad();
