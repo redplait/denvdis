7693d6ad28ff00b5d5566331305ac53f121979a5
redp <redplait@gmail.com>
2026-03-09 11:22:15 +0300

    check unvisited items too

--- test/nv_lat.cc
+++ test/nv_lat.cc
@@ -8,12 +8,21 @@ using namespace std::string_view_literals;
 
 int NV_renderer::check_lat_set(const NV_sorted *ns) const {
   int res = 0;
+  std::unordered_set<std::string_view> visited;
   for ( auto &nitem: *ns ) {
     auto fi = s_lats.find(nitem.first);
     if ( fi == s_lats.cend() ) {
       fprintf(m_out, "LMissed: %.*s\n", (int)nitem.first.size(), nitem.first.data());
       res++;
+    } else {
+      visited.insert(nitem.first);
     }
+  }
+  // dump unvisited
+  for ( auto &li: s_lats ) {
+    auto fi = visited.find(li.first);
+    if ( fi != visited.cend() ) continue;
+    fprintf(m_out, "LUnk: %.*s\n", (int)li.first.size(), li.first.data());
   }
   return res;
 }
