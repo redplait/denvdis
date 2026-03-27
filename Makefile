dce739139c88e65d980202892b2140fad1bc6f9a
redp <redplait@gmail.com>
2026-03-27 16:55:04 +0300

    BuiltinHeader

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ ELFIO=-I ../../../../ELFIO
 denv: denv.cc
 	gcc -o $@ $(ELFIO) denv.cc -lstdc++
 
+denv7: denv7.cc
+	gcc -o $@ $(ELFIO) denv7.cc -lstdc++
+
+denv11: denv11.cc
+	gcc -o $@ $(ELFIO) -I ../lz4/lib denv11.cc ../lz4/lib/liblz4.a -lstdc++
+
 deptx: ptxas.cc
 	gcc -g -o $@ $(ELFIO) ptxas.cc -lstdc++
 
@@ -14,3 +20,9 @@ cic12: cic12.cc
 
 denv12: denv12.cc
 	gcc -o $@ $(ELFIO) -I ../lz4/lib denv12.cc ../lz4/lib/liblz4.a -lstdc++
+
+nvb: nvb.cc
+	gcc -o $@ nvb.cc -ldl -lstdc++
+
+unrt: unrt.cc
+	gcc -g -o $@ $(ELFIO) unrt.cc -lstdc++
--- nvb.cc
+++ nvb.cc
@@ -6,6 +6,7 @@
 // tool to dump llvm builtins BC from libnvrtc-builtins.so.XXX
 // rdi - size_t *, rsi - arch
 typedef const unsigned char *(*Hack)(size_t *, int);
+typedef const char *(*Hdr)(size_t *);
 
 int dump(int arch, const unsigned char *bc, size_t sz) {
   std::string fn = std::to_string(arch);
@@ -21,6 +22,19 @@ int dump(int arch, const unsigned char *bc, size_t sz) {
   return 0;
 }
 
+int dump_hdr(const char *bc, size_t sz) {
+  const char *hdr_fn = "BuiltinHeader.h";
+  FILE *fp = fopen(hdr_fn, "wb");
+  if ( !fp ) {
+    fprintf(stderr, "cannot create %s, error %d (%s)\n", hdr_fn, errno, strerror(errno));
+    return errno;
+  }
+  fwrite(bc, 1, sz, fp);
+  fclose(fp);
+  return 0;
+
+}
+
 int main(int argc, char **argv) {
   if ( argc != 2 ) {
     fprintf(stderr, "where is patch to libnvrtc-builtins.so ?\n");
@@ -28,18 +42,27 @@ int main(int argc, char **argv) {
   }
   auto dl = dlopen(argv[1], RTLD_NOW);
   if ( !dl ) {
-    fprintf(stderr, "cabt open %s: %s\n", argv[1], dlerror());
+    fprintf(stderr, "cant open %s: %s\n", argv[1], dlerror());
     exit(EXIT_FAILURE);
+  }
+  size_t sz = 0;
+  Hdr gh = (Hdr)dlsym(dl, "getBuiltinHeader");
+  if ( !gh ) {
+    fprintf(stderr, "cant find hdr: %s\n", dlerror());
+  } else {
+    auto res = gh(&sz);
+    if ( res ) dump_hdr(res, sz);
+    else fprintf(stderr, "cant get hdr\n");
   }
   Hack h = (Hack)dlsym(dl, "getArchBuiltins");
   if ( !h ) {
     dlclose(dl);
-    fprintf(stderr, "cabt hack: %s\n", dlerror());
+    fprintf(stderr, "cant hack: %s\n", dlerror());
     exit(EXIT_FAILURE);
   }
   // brute-force
-  size_t sz = 0;
   for ( int arch = 1; arch <= 0x80; arch++ ) {
+    sz = 0;
     auto res = h(&sz, arch);
     if ( res ) dump(arch, res, sz);
   }
