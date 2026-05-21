928b45d298ab0b68e5381fb5c2ca5f4acb12bace
redp <redplait@gmail.com>
2026-05-21 16:25:56 +0300

    mask frequency analysis

--- cicc13/iptx.pl
+++ cicc13/iptx.pl
@@ -1,10 +1,46 @@
 #!perl -w
 # try intersect ptx.txt from cicc with ptx_ops2.txt extracted from ptxas
+# -f for mask frequency analysis
 use strict;
 use warnings;
+use Getopt::Std;
+
+# options
+use vars qw/$opt_f/;
+
+sub usage()
+{
+  print STDERR<<EOF;
+Usage: $0 [options] md.txt
+ Options:
+ - f -mask frequency analysis
+EOF
+  exit(8);
+}
 
 # instr names from ptx_ops2.txt
 my %g_ins;
+# array of ops, each element is [ line number, mask array, rest of op ]
+my @g_ops;
+
+sub do_freq
+{
+  foreach my $i ( 0 .. 15 ) {
+    foreach my $bi ( 0 .. 7 ) {
+      my $mask = 1 << $bi;
+      my $latch = 0;
+      foreach my $op ( @g_ops ) {
+        my $ar = $op->[1];
+        next unless( $ar->[$i] & $mask );
+        unless($latch) {
+          printf("idx %d bit %d:\n", $i, $bi);
+          $latch++;
+        }
+        printf(" line %d: %s\n", $op->[0], $op->[2]);
+      }
+    }
+  }
+}
 
 sub read_ops2
 {
@@ -12,16 +48,22 @@ sub read_ops2
   my @mask = ( 0 ) x16;
   my($fh, $str, $m, $rest, $iname);
   open($fh, '<', $fname) or die("Cannot open $fname, error $!");
+  my $ln = 0;
   while( $str = <$fh> ) {
     chomp $str;
+    $ln++;
     next if ( $str !~ /^\d+\s+(.*)$/ );
     $str = $1;
     # opcode name and tail starts at 48
-    $iname = (split /\t/, substr($str, 48))[0];
+    my $tail = substr($str, 48);
+    $iname = (split /\t/, $tail)[0];
     $g_ins{$iname}++; # insert into g_ins
     # make mask
     $m = substr($str, 0, 47);
     my @tmp = map { hex $_; } split /\s+/, $m;
+    if ( defined $opt_f ) {
+      push @g_ops, [ $ln, \@tmp, $tail ];
+    }
     # or with total mask
     foreach my $mi ( 0 .. 15 ) {
       $mask[$mi] |= $tmp[$mi] if $tmp[$mi];
@@ -97,5 +139,9 @@ OUTER:
 }
 
 # main
+my $status = getopts("f");
+usage() if ( !$status );
 read_ops2('ptx_ops2.txt');
-apply_ptx('ptx.txt');
+if ( defined $opt_f ) {
+  do_freq();
+} else { apply_ptx('ptx.txt'); }
