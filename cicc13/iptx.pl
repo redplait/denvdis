0be15b973cb17a829f7f89d8ac0356a65e64d8bd
redp <redplait@gmail.com>
2026-05-22 09:50:15 +0300

    -a option

--- cicc13/iptx.pl
+++ cicc13/iptx.pl
@@ -6,7 +6,7 @@ use warnings;
 use Getopt::Std;
 
 # options
-use vars qw/$opt_b $opt_f/;
+use vars qw/$opt_a $opt_b $opt_f $opt_i $opt_o/;
 
 sub usage()
 {
@@ -14,14 +14,16 @@ sub usage()
 Usage: $0 [options] md.txt
  Options:
  -b idx:shift
- -f -mask frequency analysis
+ -f - mask frequency analysis
+ -a ins1 ins2 ... - make and mask of instructions
+ -i ins1 ins2 ... - make and mask of instructions - remained
 EOF
   exit(8);
 }
 
 # instr names from ptx_ops2.txt
 my %g_ins;
-# array of ops, each element is [ line number, mask array, rest of op ]
+# array of ops, each element is [ line number, mask array, rest of op, op name ]
 my @g_ops;
 
 sub do_freq
@@ -54,6 +56,59 @@ sub try_mask
   }
 }
 
+# boring mask logic
+sub dump_mask
+{
+  my $mr = shift;
+  foreach my $mi ( @$mr ) {
+    printf("%2.2X ", $mi);
+  }
+  printf("\n");
+}
+
+sub bcnt
+{
+  my $ar = shift;
+  my $res = 0;
+  foreach my $m ( @$ar ) {
+    foreach my $i ( 0 .. 7 ) {
+      $res++ if ( $m & (1 << $i) );
+    }
+  }
+  $res;
+}
+
+sub filter_and
+{
+  my $hr = shift;
+  my @res;
+  my $found = 0;
+  foreach my $o ( @g_ops ) {
+    # check name in hash
+    next unless ( exists $hr->{ $o->[3] } );
+    my $ar = $o->[1];
+    if ( !$found ) {
+      @res = @$ar; # copy first mask array
+    } else {
+      # apply and
+      foreach my $ai ( 0 .. 15 ) {
+        $res[$ai] &= $ar->[$ai];
+      }
+    }
+    $found++;
+  }
+  if ( !$found ) {
+    printf("not found\n");
+  } else {
+    dump_mask(\@res);
+  }
+  $found;
+}
+
+sub filter_ins
+{
+}
+
 sub read_ops2
 {
   my $fname = shift;
@@ -61,6 +116,7 @@ sub read_ops2
   my($fh, $str, $m, $rest, $iname);
   open($fh, '<', $fname) or die("Cannot open $fname, error $!");
   my $ln = 0;
+  my $add = defined($opt_f) || defined($opt_b) || defined($opt_i) || defined($opt_a);
   while( $str = <$fh> ) {
     chomp $str;
     $ln++;
@@ -73,20 +129,18 @@ sub read_ops2
     # make mask
     $m = substr($str, 0, 47);
     my @tmp = map { hex $_; } split /\s+/, $m;
-    if ( defined($opt_f) || defined($opt_b) ) {
-      push @g_ops, [ $ln, \@tmp, $tail ];
-    }
+    # add to g_ops if needed
+    push @g_ops, [ $ln, \@tmp, $tail, $iname ] if ( $add );
     # or with total mask
     foreach my $mi ( 0 .. 15 ) {
       $mask[$mi] |= $tmp[$mi] if $tmp[$mi];
     }
   }
   close $fh;
-  # dump resulting mask
-  foreach my $mi ( @mask ) {
-    printf("%2.2X ", $mi);
-  }
-  printf("\n");
+  # dump results
+  dump_mask(\@mask);
+  printf("length of mask %d\n", bcnt(\@mask));
+  printf("%d uniq ins\n", scalar keys %g_ins);
 }
 
 # read ptx.txt and check in g_ins every instruction
@@ -151,11 +205,23 @@ OUTER:
 }
 
 # main
-my $status = getopts("b:f");
+my $status = getopts("b:afio");
 usage() if ( !$status );
 
 read_ops2('ptx_ops2.txt');
-if ( defined $opt_b ) {
+my %ins;
+my $ga = sub {
+  my $what = shift;
+  $ins{$_} = 1 foreach @ARGV;
+  die("no ins for $what") unless( scalar keys %ins );
+};
+if ( defined $opt_i ) {
+ $ga->('-i');
+ filter_ins(\%ins);
+} elsif ( defined $opt_a ) {
+  $ga->('-a');
+  filter_and(\%ins);
+} elsif ( defined $opt_b ) {
  # parse and check -b option
  die("bad -b option") if ( $opt_b !~ /^(\d+):(\d)$/ );
  my $idx = int($1);
